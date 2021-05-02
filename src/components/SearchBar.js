35f95c2308b37721aa21cacbb999d87e74ed6f95
builtbyjosh <pc.refuge@gmail.com>
2021-05-02 11:23:22 -0500

    configured semantic-ui on divs, added callbacks to SearchBar

--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -1,12 +1,33 @@
-import React, { Component } from 'react'
+import React, { Component } from "react";
 
 class SearchBar extends Component {
+  state = { term: "" };
+
+  onInputChange = (e) => {
+    this.setState({ term: e.target.value });
+  };
+
+  onFormSubmit = (e) => {
+    e.preventDefault()
+
+     
+  }
+
   render() {
     return (
-      <div>
-        SearchBar
+      <div className="ui segment search-bar">
+        <form action="" className="ui form" onSubmit={this.onFormSubmit}>
+          <div className="field">
+            <label htmlFor="">Video Search</label>
+            <input
+              type="text"
+              value={this.state.term}
+              onChange={this.onInputChange}
+            />
+          </div>
+        </form>
       </div>
-    )
+    );
   }
 }
-export default SearchBar
+export default SearchBar;
